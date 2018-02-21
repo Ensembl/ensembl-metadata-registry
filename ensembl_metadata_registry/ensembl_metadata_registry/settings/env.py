12209e3b3353fc5abfdfef871a5ae8c8805e7dfb
premanand17 <prem.apa@gmail.com>
2018-02-21 13:52:21 +0000

    updated the env to support multiple db settings

--- ensembl_metadata_registry/ensembl_metadata_registry/settings/env.py
+++ ensembl_metadata_registry/ensembl_metadata_registry/settings/env.py
@@ -3,5 +3,7 @@ import os
 
 ENV = os.getenv('DJANGO_ENVIRONMENT', 'development')
 DEV_ENV = ENV == 'development'
-TEST_ENV = ENV == 'staging'
+TRAVIS_ENV = ENV == 'travis'
 PROD_ENV = ENV == 'production'
+INTERNAL_PROD_ENV = ENV == 'internal_production'
+STAGING_ENV = ENV == 'staging'
