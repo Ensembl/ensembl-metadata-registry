9323de92af943ce6222dce7952aac6670613ba6e
Thomas Maurel <maurel@ebi.ac.uk>
2020-08-17 14:24:50 +0100

    Updated the migration script to use bulk_create for larger table since it's more efficient than looping through object. Added release parameter to allow more flexibility and only some releases can now be imported

--- metadata_registry/utils/schema_migration.py
+++ metadata_registry/utils/schema_migration.py
@@ -11,47 +11,18 @@ from metadata_registry_current.models.division import Division as DivisionCurren
 from metadata_registry.models.genomeinfo import Genome, GenomeAlignment, GenomeAnnotation, GenomeComparaAnalysis, GenomeDatabase, GenomeEvent, GenomeFeature, GenomeVariation, GenomeRelease, GenomeDivision 
 from metadata_registry_current.models.genomeinfo import Genome as GenomeCurrent, GenomeAlignment as GenomeAlignmentCurrent, GenomeAnnotation as GenomeAnnotationCurrent, GenomeComparaAnalysis as GenomeComparaAnalysisCurrent, GenomeDatabase as GenomeDatabaseCurrent, GenomeEvent as GenomeEventCurrent, GenomeFeature as GenomeFeatureCurrent, GenomeVariation as GenomeVariationCurrent
 
-site='RR'
-# Dealing with Assembly table
-#for AssemblyCurr in AssemblyCurrent.objects.all():
-#    NewAssembly=Assembly()
-#    NewAssembly.assembly_id=AssemblyCurr.assembly_id
-#    NewAssembly.assembly_accession=AssemblyCurr.assembly_accession
-#    NewAssembly.assembly_name=AssemblyCurr.assembly_name
-#    NewAssembly.assembly_default=AssemblyCurr.assembly_default
-#    NewAssembly.assembly_ucsc=AssemblyCurr.assembly_ucsc
-#    NewAssembly.assembly_level=AssemblyCurr.assembly_level
-#    NewAssembly.base_count=AssemblyCurr.base_count
-#    NewAssembly.save()
 
-# Dealing with Assembly sequence table
-#for AssemblySequenceCurr in AssemblySequenceCurrent.objects.all():
-#    NewAssemblySequence=AssemblySequence()
-#    NewAssemblySequence.assembly_sequence_id=AssemblySequenceCurr.assembly_sequence_id
-#    NewAssemblySequence.assembly_id=AssemblySequenceCurr.assembly_id
-#    NewAssemblySequence.name=AssemblySequenceCurr.name
-#    NewAssemblySequence.acc=AssemblySequenceCurr.acc
-#    NewAssemblySequence.save()
-
-# Dealing with Compara table
-for ComparaAnalysisCurr in ComparaAnalysisCurrent.objects.all():
-    NewComparaAnalysis=ComparaAnalysis()
-    NewComparaAnalysis.compara_analysis_id=ComparaAnalysisCurr.analysis_id
-    NewComparaAnalysis.data_release_id=ComparaAnalysisCurr.data_release_id
-    NewComparaAnalysis.division_id=ComparaAnalysisCurr.division_id
-    NewComparaAnalysis.method=ComparaAnalysisCurr.method
-    NewComparaAnalysis.set_name=ComparaAnalysisCurr.set_name
-    NewComparaAnalysis.dbname=ComparaAnalysisCurr.dbname
-    for ComparaAnalysisEventCurr in ComparaAnalysisEventCurrent.objects.all():
-        NewComparaAnalysisEvent = ComparaAnalysisEvent()
-        NewComparaAnalysisEvent.compara_analysis_event_id = ComparaAnalysisEventCurr.compara_analysis_event_id
-        NewComparaAnalysisEvent.compara_analysis = ComparaAnalysisEventCurr.compara_analysis
-        NewComparaAnalysisEvent.type = ComparaAnalysisEventCurr.type
-        NewComparaAnalysisEvent.source = ComparaAnalysisEventCurr.source
-        NewComparaAnalysisEvent.creation_time = ComparaAnalysisEventCurr.creation_time
-        NewComparaAnalysisEvent.details = ComparaAnalysisEventCurr.details
-    NewComparaAnalysis.save()
-    NewComparaAnalysisEvent.save()
+# RR or ensembl
+site='Ensembl'
+# 96 import 96
+# 96, import 96 and latest releases
+# ,96 import earliest releases up to 96
+# 96,98 import releases between 96 and 98
+release='81'
+release_list=[]
+# Use EG release ensembl_genomes_version, else use ensembl_version
+# Use EG=1 for RR or sister projects of ensembl
+eg=0
 
 # Dealing with Ensembl Site
 NewEnsemblSite=EnsemblSite()
@@ -65,22 +36,76 @@ else:
 NewEnsemblSite.save()
 
 # Dealing with data release
-for DataReleaseCurr in DataReleaseCurrent.objects.all():
+if release:
+    # Range from 96 to latest release (96,)
+    if release.endswith(','):
+        if eg:
+            Releases=DataReleaseCurrent.objects.filter(ensembl_genomes_version__gte=release[:-1])
+        else:
+            Releases=DataReleaseCurrent.objects.filter(ensembl_version__gte=release[:-1])
+    # Range from earliest to 96 (,96)
+    elif release.startswith(','):
+        if eg:
+            Releases=DataReleaseCurrent.objects.filter(ensembl_genomes_version__lte=release[1:])
+        else:
+            Releases=DataReleaseCurrent.objects.filter(ensembl_version__lte=release[1:])
+    # Range between two releases 96 and 98 (96,98)
+    elif ',' in release:
+        rel=release.split(',')
+        if eg:
+            Releases=DataReleaseCurrent.objects.filter(ensembl_genomes_version__range=(rel[0],rel[1]))
+        else:
+            Releases=DataReleaseCurrent.objects.filter(ensembl_version__range=(rel[0],rel[1]))
+    else:
+        if eg:
+            Releases=DataReleaseCurrent.objects.filter(ensembl_genomes_version=release)
+        else:
+            Releases=DataReleaseCurrent.objects.filter(ensembl_version=release)
+else:
+    Releases=DataReleaseCurrent.objects.all()
+for DataReleaseCurr in Releases:
+    release_list.append(DataReleaseCurr.data_release_id)
     NewDataRelease=DataRelease()
-    if site=='RR':
-        # For Rapid Release
+    if eg:
+        # For Rapid Release and before unified releases
         NewDataRelease.version=DataReleaseCurr.ensembl_genomes_version
     else:
         # For Main release
         NewDataRelease.version=DataReleaseCurr.ensembl_version
+    if DataReleaseCurr.ensembl_genomes_version is None:
+        NewDataRelease.label='Vert only'
     NewDataRelease.release_date=DataReleaseCurr.release_date
     NewDataRelease.is_current=DataReleaseCurr.is_current
     NewDataRelease.data_release_id = DataReleaseCurr.data_release_id
     NewDataRelease.site_id=NewEnsemblSite.site_id
     NewDataRelease.save()
 
-#Dealing with Organism, Organism_alias and organism_publcation tables
-for OrganismCurr in OrganismCurrent.objects.all():
+# Populating division table
+DivisionCurr=DivisionCurrent.objects.all()
+Division.objects.bulk_create(DivisionCurr)
+
+# Dealing with Compara table
+ComparaAnalysisCurr=ComparaAnalysisCurrent.objects.filter(data_release_id__in=release_list)
+ComparaAnalysis.objects.bulk_create(ComparaAnalysisCurr)
+ComparaAnalysisEventCurr=ComparaAnalysisEventCurrent.objects.filter(compara_analysis_id__in=ComparaAnalysisCurr)
+ComparaAnalysisEvent.objects.bulk_create(ComparaAnalysisEventCurr)
+
+# Populating Genome table and associated genome_* tables
+for GenomeCurr in GenomeCurrent.objects.filter(data_release_id__in=release_list):
+    NewGenome=Genome()
+    NewGenome.genome_id = GenomeCurr.genome_id
+    NewGenome.genebuild = GenomeCurr.genebuild
+    NewGenome.has_pan_compara = GenomeCurr.has_pan_compara
+    NewGenome.has_variation = GenomeCurr.has_variations
+    #NewGenome.has_microarray = GenomeCurr.has_microarray
+    NewGenome.has_microarray=0
+    NewGenome.has_peptide_compara = GenomeCurr.has_peptide_compara
+    NewGenome.has_genome_alignments = GenomeCurr.has_genome_alignments
+    NewGenome.has_synteny = GenomeCurr.has_synteny
+    NewGenome.has_other_alignments = GenomeCurr.has_other_alignments
+    NewGenome.created = GenomeEventCurrent.objects.filter(genome_id=NewGenome.genome_id).last().creation_time
+    #Dealing with Organism, Organism_alias and organism_publcation tables
+    OrganismCurr=OrganismCurrent.objects.filter(organism_id=GenomeCurr.organism_id).first()
     NewOrganism=Organism()
     NewOrganism.organism_id = OrganismCurr.organism_id
     NewOrganism.taxonomy_id = OrganismCurr.taxonomy_id
@@ -94,112 +119,71 @@ for OrganismCurr in OrganismCurrent.objects.all():
     NewOrganism.image = OrganismCurr.image
     NewOrganism.scientific_name = OrganismCurr.scientific_name
     NewOrganism.url_name = OrganismCurr.url_name
-    NewOrganism.save()
-    for OrganismAliasCurr in OrganismAliasCurrent.objects.filter(organism_id=NewOrganism.organism_id):
-        NewOrganismAlias=OrganismAlias()
-        NewOrganismAlias.organism_alias_id = OrganismAliasCurr.organism_alias_id
-        NewOrganismAlias.organism = NewOrganism
-        NewOrganismAlias.alias = OrganismAliasCurr.alias
-        NewOrganismAlias.save()
-    for OrganismPublicationCurr in OrganismPublicationCurrent.objects.filter(organism_id=NewOrganism.organism_id):
-        NewOrganismPublication = OrganismPublication()
-        NewOrganismPublication.organism_publication_id = OrganismPublicationCurr.organism_publication_id
-        NewOrganismPublication.organism = NewOrganism
-        NewOrganismPublication.publication = OrganismPublicationCurr.publication
-        NewOrganismPublication.save()
-    
-# Populating new group table
-for OrganismCurr in OrganismCurrent.objects.exclude(reference__isnull=True).exclude(reference=''):
+    # Populating new group table
     NewOrganismGroup=OrganismGroup()
     NewOrganismGroup.type='strains'
     NewOrganismGroup.reference_organism = Organism.objects.filter(name=OrganismCurr.reference).first()
     NewOrganismGroup.save()
-    NewOrganism = Organism.objects.filter(organism_id=OrganismCurr.organism_id).first()
     NewOrganism.group = NewOrganismGroup
     NewOrganism.save()
-    
-# Populating division table
-for DivisionCurr in DivisionCurrent.objects.all():
+    # Deal with organism Alias
+    OrganismAliasCurr=OrganismAliasCurrent.objects.filter(organism_id=NewOrganism.organism_id)
+    OrganismAlias.objects.bulk_create(OrganismAliasCurr)
+    # Deal with organim publication
+    OrganismPublicationCurr=OrganismPublicationCurrent.objects.filter(organism_id=NewOrganism.organism_id)
+    OrganismPublication.objects.bulk_create(OrganismPublicationCurr)
+    NewGenome.organism = NewOrganism
+    # Dealing with Assembly table
+    # Populating AssemblySequence table is time consuming so better check if data exist first
+    # Since it's shared between releases
+    CheckAssemblyExist=Assembly.objects.filter(assembly_id=GenomeCurr.assembly_id)
+    if CheckAssemblyExist.count() == 0:
+        AssemblyCurr=AssemblyCurrent.objects.filter(assembly_id=GenomeCurr.assembly_id).first()
+        NewAssembly=Assembly()
+        NewAssembly.assembly_id=AssemblyCurr.assembly_id
+        NewAssembly.assembly_accession=AssemblyCurr.assembly_accession
+        NewAssembly.assembly_name=AssemblyCurr.assembly_name
+        NewAssembly.assembly_default=AssemblyCurr.assembly_default
+        NewAssembly.assembly_ucsc=AssemblyCurr.assembly_ucsc
+        NewAssembly.assembly_level=AssemblyCurr.assembly_level
+        NewAssembly.base_count=AssemblyCurr.base_count
+        NewAssembly.save()
+        # Dealing with Assembly sequence table
+        AssemblySequenceCurr=AssemblySequenceCurrent.objects.filter(assembly_id=AssemblyCurr.assembly_id)
+        AssemblySequence.objects.bulk_create(AssemblySequenceCurr)
+        NewGenome.assembly=NewAssembly
+        NewGenome.save()
+    else:
+        NewGenome.assembly=CheckAssemblyExist.first()
+        NewGenome.save()
+    # Dealing with Genome databases
+    GenomeDatabaseCurr=GenomeDatabaseCurrent.objects.filter(genome_id=NewGenome.genome_id)
+    GenomeDatabase.objects.bulk_create(GenomeDatabaseCurr)
+    # Dealing with Genome alignement
+    GenomeAlignmentCurr=GenomeAlignmentCurrent.objects.filter(genome_id=NewGenome.genome_id)
+    GenomeAlignment.objects.bulk_create(GenomeAlignmentCurr)
+    # Dealing with Genome annotation
+    GenomeAnnotationCurr=GenomeAnnotationCurrent.objects.filter(genome_id=NewGenome.genome_id)
+    GenomeAnnotation.objects.bulk_create(GenomeAnnotationCurr)
+    # Dealing with Genome Features
+    GenomeFeatureCurr=GenomeFeatureCurrent.objects.filter(genome_id=NewGenome.genome_id)
+    GenomeFeature.objects.bulk_create(GenomeFeatureCurr)
+    # Dealing with Genome Variation
+    GenomeVariationCurr=GenomeVariationCurrent.objects.filter(genome_id=NewGenome.genome_id)
+    GenomeVariation.objects.bulk_create(GenomeVariationCurr)
+    # Dealing with Genome Compara analysis
+    GenomeComparaAnalysisCurr=GenomeComparaAnalysisCurrent.objects.filter(genome_id=NewGenome.genome_id)
+    GenomeComparaAnalysis.objects.bulk_create(GenomeComparaAnalysisCurr)
+    # Dealing with Genome Event
+    GenomeEventCurr=GenomeEventCurrent.objects.filter(genome_id=NewGenome.genome_id)
+    GenomeEvent.objects.bulk_create(GenomeEventCurr)
+    # Populating division table
+    DivisionCurr=DivisionCurrent.objects.filter(division_id=GenomeCurr.division_id).first()
     NewDivision=Division()
     NewDivision.division_id = DivisionCurr.division_id
     NewDivision.name = DivisionCurr.name
     NewDivision.short_name = DivisionCurr.short_name
     NewDivision.save()
-    
-# Populating Genome table and associated genome_* tables
-for GenomeCurr in GenomeCurrent.objects.all():
-    NewGenome=Genome()
-    NewGenome.genome_id = GenomeCurr.genome_id
-    NewGenome.assembly = Assembly.objects.filter(assembly_id=GenomeCurr.assembly_id).first()
-    NewGenome.organism = Organism.objects.filter(organism_id=GenomeCurr.organism_id).first()
-    NewGenome.genebuild = GenomeCurr.genebuild
-    NewGenome.has_pan_compara = GenomeCurr.has_pan_compara
-    NewGenome.has_variation = GenomeCurr.has_variations
-    #NewGenome.has_microarray = GenomeCurr.has_microarray
-    NewGenome.has_microarray=0
-    NewGenome.has_peptide_compara = GenomeCurr.has_peptide_compara
-    NewGenome.has_genome_alignments = GenomeCurr.has_genome_alignments
-    NewGenome.has_synteny = GenomeCurr.has_synteny
-    NewGenome.has_other_alignments = GenomeCurr.has_other_alignments
-    NewGenome.created = GenomeEventCurrent.objects.filter(genome_id=NewGenome.genome_id).last().creation_time
-    NewGenome.save()
-    for GenomeDatabaseCurr in GenomeDatabaseCurrent.objects.filter(genome_id=NewGenome.genome_id):
-        NewGenomeDatabase = GenomeDatabase()
-        NewGenomeDatabase.genome_database_id = GenomeDatabaseCurr.genome_database_id
-        NewGenomeDatabase.genome = NewGenome
-        NewGenomeDatabase.dbname = GenomeDatabaseCurr.dbname
-        NewGenomeDatabase.species_id = GenomeDatabaseCurr.species_id
-        NewGenomeDatabase.type = GenomeDatabaseCurr.type
-        NewGenomeDatabase.save()
-        for GenomeAlignmentCurr in GenomeAlignmentCurrent.objects.filter(genome_id=NewGenome.genome_id):
-            MewGenomeAlignment = GenomeAlignment()
-            MewGenomeAlignment.genome_alignment_id = GenomeAlignmentCurr.genome_alignment_id
-            MewGenomeAlignment.genome = NewGenome
-            MewGenomeAlignment.type = GenomeAlignmentCurr.type
-            MewGenomeAlignment.name = GenomeAlignmentCurr.name
-            MewGenomeAlignment.count = GenomeAlignmentCurr.count
-            MewGenomeAlignment.genome_database = NewGenomeDatabase
-            MewGenomeAlignment.save()
-        for GenomeAnnotationCurr in GenomeAnnotationCurrent.objects.filter(genome_id=NewGenome.genome_id):
-            NewGenomeAnnotation = GenomeAnnotation()
-            NewGenomeAnnotation.genome_annotation_id = GenomeAnnotationCurr.genome_annotation_id
-            NewGenomeAnnotation.genome = NewGenome
-            NewGenomeAnnotation.type = GenomeAnnotationCurr.type
-            NewGenomeAnnotation.value = GenomeAnnotationCurr.value
-            NewGenomeAnnotation.genome_database = NewGenomeDatabase
-            NewGenomeAnnotation.save()
-        for GenomeFeatureCurr in GenomeFeatureCurrent.objects.filter(genome_id=NewGenome.genome_id):
-            NewGenomeFeature = GenomeFeature()
-            NewGenomeFeature.genome_feature_id = GenomeFeatureCurr.genome_feature_id
-            NewGenomeFeature.genome = NewGenome
-            NewGenomeFeature.type = GenomeFeatureCurr.type
-            NewGenomeFeature.analysis = GenomeFeatureCurr.analysis
-            NewGenomeFeature.count = GenomeFeatureCurr.count
-            NewGenomeFeature.genome_database = NewGenomeDatabase
-            NewGenomeFeature.save()
-        for GenomeVariationCurr in GenomeVariationCurrent.objects.filter(genome_id=NewGenome.genome_id):
-            NewGenomeVariation = GenomeVariation()
-            NewGenomeVariation.genome_variation_id = GenomeVariationCurr.genome_variation_id
-            NewGenomeVariation.genome = NewGenome
-            NewGenomeVariation.type = GenomeVariationCurr.type
-            NewGenomeVariation.name = GenomeVariationCurr.name
-            NewGenomeVariation.count = GenomeVariationCurr.count
-            NewGenomeVariation.genome_database = NewGenomeDatabase
-            NewGenomeVariation.save()
-    for GenomeComparaAnalysisCurr in GenomeComparaAnalysisCurrent.objects.filter(genome_id=NewGenome.genome_id):
-        NewGenomeComparaAnalysis = GenomeComparaAnalysis()
-        NewGenomeComparaAnalysis.genome_compara_analysis_id = GenomeComparaAnalysisCurr.genome_compara_analysis_id
-        NewGenomeComparaAnalysis.genome = NewGenome
-        NewGenomeComparaAnalysis.compara_analysis = GenomeComparaAnalysisCurr.compara_analysis
-        NewGenomeComparaAnalysis.save()
-    for GenomeEventCurr in GenomeEventCurrent.objects.filter(genome_id=NewGenome.genome_id):
-        NewGenomeEvent = GenomeEvent()
-        NewGenomeEvent.genome = NewGenome
-        NewGenomeEvent.type = GenomeEventCurr.type
-        NewGenomeEvent.source = GenomeEventCurr.source
-        NewGenomeEvent.creation_time = GenomeEventCurr.creation_time
-        NewGenomeEvent.details = GenomeEventCurr.details
-        NewGenomeEvent.save()
     # Dealing with new Genome Division table
     NewGenomeDivision = GenomeDivision()
     NewGenomeDivision.division = Division.objects.filter(division_id=GenomeCurr.division_id).first()
