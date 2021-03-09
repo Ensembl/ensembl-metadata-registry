61d1d8df46c31d82471a39bd09d6f96cb2f8abe6
James Allen <jallen@ebi.ac.uk>
2021-03-09 14:45:25 +0000

    Upgrade to latest Django+packages; propagate revised Django project name.

--- metadata_registry/settings/env.py
+++ metadata_registry/settings/env.py
@@ -4,7 +4,8 @@ ENV = os.getenv('DJANGO_ENVIRONMENT', 'development')
 DEV_ENV = ENV == 'development'
 PROD_ENV = ENV == 'production'
 
+REGISTRY_DB = os.getenv('REGISTRY_DB', 'metadata_registry')
 METADATA_DB = os.getenv('METADATA_DB', 'ensembl_metadata')
-TAXONOMY_DB = os.getenv('METADATA_DB', 'ncbi_taxonomy')
+TAXONOMY_DB = os.getenv('TAXONOMY_DB', 'ncbi_taxonomy')
 
 METADATA_DB_OLD = os.getenv('METADATA_DB_OLD')
--- metadata_registry/settings/secrets_template.py
+++ metadata_registry/settings/secrets_template.py
@@ -1,13 +1,13 @@
-import ensembl_metadata.settings.env
+import metadata_registry.settings.env
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = ''
+SECRET_KEY = 'replace_me'
 
-if ensembl_metadata.settings.env.DEV_ENV:
+if metadata_registry.settings.env.DEV_ENV:
     DATABASE_USER = 'readuser'
     DATABASE_PASSWORD = 'password'
     DATABASE_HOST = 'localhost'
     DATABASE_PORT = '3306'
-elif ensembl_metadata.settings.env.PROD_ENV:
+elif metadata_registry.settings.env.PROD_ENV:
     DATABASE_USER = 'readuser'
     DATABASE_PASSWORD = 'password'
     DATABASE_HOST = 'host'
