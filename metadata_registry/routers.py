77b2294e97e43ffa5bdea5c3e19a3c25ec9bfa30
James Allen <jallen@ebi.ac.uk>
2021-03-09 14:54:51 +0000

    Reflect updated project name. Remove datatable urls. Tidy up database routing.

--- metadata_registry/routers.py
+++ metadata_registry/routers.py
@@ -1,63 +1,41 @@
-"""
-.. See the NOTICE file distributed with this work for additional information
-   regarding copyright ownership.
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-       http://www.apache.org/licenses/LICENSE-2.0
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-"""
 
-
-class EnsemblMetaDataRegistryRouter(object):
+class MetaDataRegistryRouter(object):
     """
-    A router to control all database operations on models in the
-    ensembl metadata registry.
+    A router to direct operations in the metadata registry to either
+    the metadata or taxonomy databases.
     """
-    META_DBS = ('metadata_registry')
+    app_labels = {'ensembl_metadata', 'ncbi_taxonomy'}
 
     def db_for_read(self, model, **hints):
-        """
-        Attempts to read ncbi_taxonomy models go to ncbi_taxonomy.
-        """
         if model._meta.app_label == 'ncbi_taxonomy':
             return 'ncbi_taxonomy'
-        if model._meta.app_label == 'metadata_registry':
+        if model._meta.app_label == 'ensembl_metadata':
             return 'ensembl_metadata'
         return None
 
     def db_for_write(self, model, **hints):
-        """
-        Attempts to write to dbs go to default.
-        """
         if model._meta.app_label == 'ncbi_taxonomy':
             return 'ncbi_taxonomy'
-        if model._meta.app_label == 'metadata_registry':
+        if model._meta.app_label == 'ensembl_metadata':
             return 'ensembl_metadata'
         return None
 
     def allow_relation(self, obj1, obj2, **hints):
         """
-
+        Within db relations are fine, also allow links between
+        metadata and taxonomy databases.
         """
-        if obj1._meta.app_label == 'metadata_registry' or \
-           obj2._meta.app_label == 'metadata_registry':
-            return True
-        if obj1._meta.app_label in EnsemblMetaDataRegistryRouter.META_DBS or \
-           obj2._meta.app_label in EnsemblMetaDataRegistryRouter.META_DBS:
+        if obj1._meta.app_label in self.app_labels and \
+           obj2._meta.app_label in self.app_labels:
             return True
         return None
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         """
-
+        Migrations for a specific app are restricted to the appropriate database.
         """
         if app_label == 'ncbi_taxonomy':
             return db == 'ncbi_taxonomy'
-        if app_label == 'metadata_registry':
+        if app_label == 'ensembl_metadata':
             return db == 'ensembl_metadata'
         return None
--- metadata_registry/urls.py
+++ metadata_registry/urls.py
@@ -1,87 +1,28 @@
-"""
-.. See the NOTICE file distributed with this work for additional information
-   regarding copyright ownership.
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-       http://www.apache.org/licenses/LICENSE-2.0
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-"""
-
-from django.conf import settings
-from django.conf.urls import url, include
 from django.contrib import admin
-from django.views.generic.base import TemplateView
-from rest_framework_swagger.views import get_swagger_view
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.urls import include, path
+from django.views.generic.base import RedirectView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
-from metadata_registry.views.assembly import AssemblyDatatableView
-from metadata_registry.views.base import datatable_view, datatable_fetch
-from metadata_registry.views.datarelease import DataReleaseDatatableView
-from metadata_registry.views.division import DivisionDatatableView
-from metadata_registry.views.genomeinfo import GenomeDatatableView
-from metadata_registry.views.organism import OrganismDatatableView
-
-emr_apis = [
-    url(r'^assembly/', include('metadata_registry.urls.assembly')),
-    url(r'^compara_analysis/', include('metadata_registry.urls.compara')),
-    url(r'^datarelease/', include('metadata_registry.urls.datarelease')),
-    url(r'^division/', include('metadata_registry.urls.division')),
-    url(r'^genome/', include('metadata_registry.urls.genomeinfo')),
-    url(r'^ncbi_taxonomy/', include('ncbi_taxonomy.urls')),
-    url(r'^organism/', include('metadata_registry.urls.organism')),
-    url(r'^meta_stats/', include('metadata_registry.urls.meta')),
-]
-
-schema_view = get_swagger_view(title='Ensembl Metadata Registry REST API Endpoints', patterns=emr_apis)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^docs/', schema_view, name='doc_view'),
-    url(r'^$', schema_view, name='home_view'),
-
+    path('admin/', admin.site.urls),
+    path('assembly/', include('ensembl_metadata.urls.assembly')),
+    path('genome/', include('ensembl_metadata.urls.genome')),
+    path('organism/', include('ensembl_metadata.urls.organism')),
+    path('release/', include('ensembl_metadata.urls.datarelease')),
+    path('taxonomy/', include('ncbi_taxonomy.urls')),
+    path(
+        'schema/',
+        SpectacularAPIView.as_view(),
+        name='schema'),
+    path(
+        'docs/',
+        SpectacularSwaggerView.as_view(
+            template_name='swagger-ui.html',
+            url_name='schema',
+        ),
+        name='swagger-ui',
+    ),
+    path('', RedirectView.as_view(url='docs/')),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico'))),
 ]
-
-internal_apis = [
-    # for datatables
-    url(r'^datatable/(?P<table_name>[\w]+)/', datatable_view, name="datatable_view"),
-
-    # FIXME this url doens't work but doesn't seem to be used anywhere, anyway.
-    # url(r'^datatable_clientside/(?P<table_name>[\w]+)/', datatable_fetch, name="datatablefetch_clientside"),
-
-    url(r'^datatable_serverside/assembly', AssemblyDatatableView.as_view(),
-        name="datatablefetch_serverside_assembly"),
-
-    url(r'^datatable_serverside/organism', OrganismDatatableView.as_view(),
-        name="datatablefetch_serverside_organism"),
-
-    url(r'^datatable_serverside/genome', GenomeDatatableView.as_view(),
-        name="datatablefetch_serverside_genome"),
-
-    url(r'^datatable_serverside/division', DivisionDatatableView.as_view(),
-        name="datatablefetch_serverside_division"),
-
-    url(r'^datatable_serverside/datarelease', DataReleaseDatatableView.as_view(),
-        name="datatablefetch_serverside_datarelease"),
-    url(r'^privacy_notice_emr', TemplateView.as_view(template_name='metadata_registry/privacy_notice_emr.html'),
-        name="privacy_note_emr"),
-    # for datatables
-    url(r'^datatable/(?P<table_name>[\w]+)/', datatable_view, name="datatable_view"),
-
-    # FIXME this url doens't work but doesn't seem to be used anywhere, anyway.
-    # url(r'^datatable_clientside/(?P<table_name>[\w]+)/', datatable_fetch, name="datatablefetch_clientside"),
-]
-
-urlpatterns = urlpatterns + emr_apis + internal_apis
-
-if settings.DEBUG:
-    import debug_toolbar
-
-    urlpatterns = [
-                      url(r'^__debug__/', include(debug_toolbar.urls)),
-                  ] + urlpatterns
