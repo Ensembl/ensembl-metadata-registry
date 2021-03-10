738b7379212fc058be2dfa4ccf38144c25289a51
James Allen <jallen@ebi.ac.uk>
2021-03-10 12:34:20 +0000

    Simplified AssemblySequence.assembly related_name. Removed redundant 'unique_together', which used assembly_accession - that's already defined as unique.

--- ensembl_metadata/models/assembly.py
+++ ensembl_metadata/models/assembly.py
@@ -1,24 +1,7 @@
-"""
-.. See the NOTICE file distributed with this work for additional information
-   regarding copyright ownership.
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-       http://www.apache.org/licenses/LICENSE-2.0
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-"""
-
 from django.db import models
 
 
 class Assembly(models.Model):
-
-    ONE2MANY_RELATED = {'ASSEMBLY_SEQUENCE': 'assembly_sequence', 'ASSEMBLY_KARYOTYPE': 'assembly_karyotype'}
-
     assembly_id = models.AutoField(primary_key=True)
     assembly_accession = models.CharField(unique=True, max_length=16, blank=True, null=True)
     assembly_name = models.CharField(max_length=200)
@@ -30,13 +13,11 @@ class Assembly(models.Model):
     class Meta:
         managed = True
         db_table = 'assembly'
-        unique_together = (('assembly_accession', 'assembly_default', 'base_count'),)
+
 
 class AssemblySequence(models.Model):
     assembly_sequence_id = models.AutoField(primary_key=True)
-    assembly = models.ForeignKey(Assembly, models.CASCADE,
-                                 related_name=Assembly.ONE2MANY_RELATED['ASSEMBLY_SEQUENCE'])
-
+    assembly = models.ForeignKey(Assembly, on_delete=models.CASCADE, related_name='sequences')
     name = models.CharField(max_length=40)
     acc = models.CharField(max_length=24, blank=True, null=True)
 
@@ -48,8 +29,7 @@ class AssemblySequence(models.Model):
 
 class AssemblyKaryotype(models.Model):
     assembly_karyotype_id = models.AutoField(primary_key=True)
-    assembly = models.ForeignKey(Assembly, models.CASCADE,
-                                 related_name=Assembly.ONE2MANY_RELATED['ASSEMBLY_KARYOTYPE'])
+    assembly = models.ForeignKey(Assembly, models.CASCADE, related_name='karyotypes')
     region_name = models.CharField(max_length=40)
     region_start = models.IntegerField()
     region_end = models.IntegerField()
