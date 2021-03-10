2fbf421cb0bdbeafbf9ac85ca2e2a79018453e81
James Allen <jallen@ebi.ac.uk>
2021-03-10 12:13:50 +0000

    Moving 'compara' and 'division' into genome serializer; and 'site' into datarelease serializer. Simplified all serializers by removing the drf mixin code - this made the fk links hard to understand/maintain.

--- ensembl_metadata/api/datarelease/serializers.py
+++ ensembl_metadata/api/datarelease/serializers.py
@@ -1,22 +1,16 @@
-"""
-.. See the NOTICE file distributed with this work for additional information
-   regarding copyright ownership.
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-       http://www.apache.org/licenses/LICENSE-2.0
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-"""
-
 from rest_framework import serializers
-from metadata_registry.models.datarelease import DataRelease
+from ensembl_metadata.models.datarelease import DataRelease, Site
+
+
+class SiteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Site
+        exclude = ['site_id']
 
 
 class DataReleaseSerializer(serializers.ModelSerializer):
+    site = SiteSerializer(many=False, required=True)
+
     class Meta:
         model = DataRelease
-        fields = '__all__'
+        exclude = ['data_release_id', 'label']
--- ensembl_metadata/api/genome/serializers.py
+++ ensembl_metadata/api/genome/serializers.py
@@ -1,27 +1,32 @@
-"""
-.. See the NOTICE file distributed with this work for additional information
-   regarding copyright ownership.
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-       http://www.apache.org/licenses/LICENSE-2.0
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-"""
-
-
-from metadata_registry.models.genomeinfo import Genome, GenomeAlignment, GenomeAnnotation, GenomeComparaAnalysis, \
-                              GenomeDatabase, GenomeEvent, GenomeFeature, GenomeVariation
 from rest_framework import serializers
-from metadata_registry.api.compara.serializers import ComparaAnalysisSerializer
-from metadata_registry.api.organism.serializers import OrganismSerializer
-from metadata_registry.api.datarelease.serializers import DataReleaseSerializer
-from metadata_registry.api.assembly.serializers import AssemblySerializer
-from metadata_registry.api.division.serializers import DivisionSerializer
-from metadata_registry.utils.drf_mixin import SerializerMixin
+from ensembl_metadata.api.assembly.serializers import AssemblySerializer
+from ensembl_metadata.api.organism.serializers import OrganismSerializer
+from ensembl_metadata.models.genome import \
+    Division, Genome, GenomeAlignment, GenomeAnnotation, GenomeComparaAnalysis, \
+    GenomeDatabase, GenomeEvent, GenomeFeature, GenomeVariation, \
+    ComparaAnalysis, ComparaAnalysisEvent
+
+
+class DivisionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Division
+        fields = '__all__'
+
+
+class GenomeDatabaseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GenomeDatabase
+        exclude = ['genome_database_id', 'genome', 'species_id']
+
+
+class GenomeSerializer(serializers.ModelSerializer):
+    assembly = AssemblySerializer(many=False, required=False)
+    organism = OrganismSerializer(many=False, required=False)
+    databases = GenomeDatabaseSerializer(many=True, required=False)
+
+    class Meta:
+        model = Genome
+        exclude = ['genome_id', 'created']
 
 
 class GenomeAlignmentSerializer(serializers.ModelSerializer):
@@ -33,13 +38,14 @@ class GenomeAlignmentSerializer(serializers.ModelSerializer):
 class GenomeAnnotationSerializer(serializers.ModelSerializer):
     class Meta:
         model = GenomeAnnotation
-        fields = '__all__'
+        fields = ['type', 'value']
 
 
-class GenomeDatabaseSerializer(serializers.ModelSerializer):
+class GenomeComparaAnalysisSerializer(serializers.ModelSerializer):
+
     class Meta:
-        model = GenomeDatabase
-        fields = '__all__'
+        model = GenomeComparaAnalysis
+        fields = ('genome', 'compara_analysis')
 
 
 class GenomeEventSerializer(serializers.ModelSerializer):
@@ -60,35 +66,13 @@ class GenomeVariationSerializer(serializers.ModelSerializer):
         fields = ('type', 'name', 'count')
 
 
-class GenomeSerializer(SerializerMixin, serializers.ModelSerializer,):
-
-    """
-    A ModelSerializer that controls which fields should be displayed.
-    """
-    MANY2ONE_SERIALIZER = {Genome.MANY2ONE_RELATED['ASSEMBLY']: AssemblySerializer,
-                           Genome.MANY2ONE_RELATED['ORGANISM']: OrganismSerializer,
-                           Genome.MANY2ONE_RELATED['DIVISION']: DivisionSerializer}
-
-    ONE2MANY_SERIALIZER = {Genome.ONE2MANY_RELATED['GENOME_ALIGNMENT']: GenomeAlignmentSerializer,
-                           Genome.ONE2MANY_RELATED['GENOME_ANNOTATION']: GenomeAnnotationSerializer,
-                           Genome.ONE2MANY_RELATED['GENOME_DATABASE']: GenomeDatabaseSerializer,
-                           Genome.ONE2MANY_RELATED['GENOME_EVENT']: GenomeEventSerializer,
-                           Genome.ONE2MANY_RELATED['GENOME_FEATURE']: GenomeFeatureSerializer,
-                           Genome.ONE2MANY_RELATED['GENOME_VARIATION']: GenomeVariationSerializer,
-                           Genome.ONE2MANY_RELATED['GENOME_COMPARA_ANALYSIS']: ComparaAnalysisSerializer
-                           }
-
+class ComparaAnalysisSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Genome
+        model = ComparaAnalysis
         fields = '__all__'
 
-    def __init__(self, *args, **kwargs):
-        super(GenomeSerializer, self).__init__(*args, **kwargs)
-        self.set_related_fields(GenomeSerializer, **kwargs)
 
-
-class GenomeComparaAnalysisSerializer(serializers.ModelSerializer):
-
+class ComparaAnalysisEventSerializer(serializers.ModelSerializer):
     class Meta:
-        model = GenomeComparaAnalysis
-        fields = ('genome', 'compara_analysis')
+        model = ComparaAnalysisEvent
+        fields = '__all__'
